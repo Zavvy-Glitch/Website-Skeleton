ef0db5039643a7e6177af37ced9d2ce73a199e72
chattray <chea0073@gmail.com>
2021-05-27 12:06:59 -0700

    Inline CSS and Java Functions

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-// let userName = 'Tray'
-// userName = 'Donatella'
-// console.log(userName)
-// userName = 'Crazyhorse'
-// console.log(userName)
-
-userName =  prompt('Please Enter Your Name: ');
-console.log(userName);
-
-
-userAnswer = prompt('What is Cleopatras favorite perfume?');
-console.log(userAnswer);
-
-if (userAnswer == 'I dont know') {
-    document.write('<h2>' + userName + ',                                             Pharoah mone</h2>');
-
+function getUserName(){
+    let userName = prompt('Please Enter Your Name: ');
+    console.log(userName);
+    document.write('<h2>' + userName + '</h2>');
+    return userName; //
 }
 
-if (userAnswer == 'Pharoah mone') {
-    document.write('<h2>' + userName +' ,wow you are smart!</h2>');
+function getUserAnswer(){
+    let userAnswer = prompt('Do you know Cleopatras favorite perfume is? (Yes or No)');
+    console.log(userAnswer);
+
+    if (userAnswer.toLowerCase() == 'yes') {
+        document.write('<h4>LOL, Pharoahmones!</h>');
+    } 
+      else {
+        document.write('<h3>Its ok, it was a bad joke anyways.</h3>');
+    }
+}
+
+function cleopatrashead(){
+    let cleopatrashead = prompt('Do you think Cleos statues are all wrong?')
+    let url ="https://upload.wikimedia.org/wikipedia/commons/8/8f/Bust_of_Cleopatra_at_the_Royal_Ontario_Museum.jpg"
+    if (cleopatrashead.toLowerCase() == 'yes'){
+        document.write('<h3>Who knows? Its made of stone.</h3>');
+        document.write('<img src="' + url + '"/>')
+    }
 }
